00222238318b686a07cabf5b32e28be08003704a
Syafiqq <syafiq.rezpector@gmail.com>
2017-04-29 08:17:06 +0700

    Differentiate student and counselor operation

--- application/models/Mauth.php
+++ application/models/Mauth.php
@@ -16,18 +16,61 @@ class Mauth extends CI_Model
         // Your own constructor code
     }
 
-    public function findByEmailAndRole($email, $role)
+    public function findStudentByCredential($credential)
     {
-        $query = 'SELECT `id`, `name`, `email`, `role`, `gender`, `password` FROM `users` WHERE `email` = ? AND `role` = ? LIMIT 1';
-        $result = $this->db->query($query, array((string)$email, (string)$role));
+        $query = 'SELECT `id`, `credential`, `name`, `gender`, `password`, `period`, `grade`, `address`, `birthplace`, `datebirth`, `avatar`, `on_create`, `on_update` FROM `user_student` WHERE `credential` = ?';
+        $result = $this->db->query($query, array((string)$credential));
 
         return $result->result_array();
     }
 
-    public function register($name, $email, $role, $gender, $password)
+    public function findCounselorByCredential($credential)
     {
-        $query = 'INSERT INTO `users`(`id`, `name`, `email`, `role`, `gender`, `password`) VALUES (NULL, ?, ?, ?, ?, ?)';
-        $this->db->query($query, array((string)$name, (string)$email, (string)$role, (string)$gender, (string)$password));
+        $query = 'SELECT `id`, `credential`, `name`, `gender`, `password`, `address`, `birthplace`, `datebirth`, `avatar`, `on_create`, `on_update` FROM `user_counselor` WHERE `credential` = ?';
+        $result = $this->db->query($query, array((string)$credential));
+
+        return $result->result_array();
+    }
+
+    public function registerStudent($name, $credential, $gender, $password)
+    {
+        $avatar = $this->generateDefaultAvatar($gender);
+        $query = 'INSERT INTO `user_student`(`id`, `credential`, `name`, `gender`, `password`, `period`, `grade`, `address`, `birthplace`, `datebirth`, `avatar`, `on_create`, `on_update`) VALUES (NULL, ?, ?, ?, ?, NULL, NULL, NULL, NULL, NULL, ?, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);';
+        $this->db->query($query, array((string)$credential, (string)$name, (string)$gender, (string)$password, (string)$avatar));
+
+        return $this->db->insert_id();
+    }
+
+    private function generateDefaultAvatar($gender = 'male')
+    {
+        switch ($gender)
+        {
+            case 'male' :
+            {
+                $avatar = mt_rand(0, 22);
+                $avatar = "/assets/img/avatar/male/boy-{$avatar}.png";
+            }
+                break;
+            case 'female' :
+            {
+                $avatar = mt_rand(0, 26);
+                $avatar = "/assets/img/avatar/female/girl-{$avatar}.png";
+            }
+                break;
+            default :
+            {
+                $avatar = '/assets/img/avatar/default/default.png';
+            }
+        }
+
+        return $avatar;
+    }
+
+    public function registerCounselor($name, $credential, $gender, $password)
+    {
+        $avatar = $this->generateDefaultAvatar($gender);
+        $query = 'INSERT INTO `user_counselor`(`id`, `credential`, `name`, `gender`, `password`, `address`, `birthplace`, `datebirth`, `avatar`, `on_create`, `on_update`) VALUES (NULL, ?, ?, ?, ?, NULL, NULL, NULL, ?, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);';
+        $this->db->query($query, array((string)$credential, (string)$name, (string)$gender, (string)$password, (string)$avatar));
 
         return $this->db->insert_id();
     }
