528d2c4a3a1627798eebcf76ce51583b99eb3ef0
Syafiqq <syafiq.rezpector@gmail.com>
2017-05-02 11:35:52 +0700

    Add recover student layout

---
+++ application/views/auth/recover/recover-student.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * This <angket.000.app> project created by :
+ * Name         : syafiq
+ * Date / Time  : 02 May 2017, 9:57 AM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+?>
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Recover</title>
+    <meta name="a temlplate" content="">
+
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo base_url('/apple-touch-icon.png') ?>">
+    <!-- Place favicon.ico in the root directory -->
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="<?php echo base_url('/assets/css/normalize.css') ?>">
+    <link rel="stylesheet" href="<?php echo base_url('/assets/css/main.css') ?>">
+
+    <link href="<?php echo base_url('/assets/bower_components/bootstrap/dist/css/bootstrap.min.css') ?>" rel="stylesheet">
+    <link href="<?php echo base_url('/assets/bower_components/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css') ?>" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="<?php echo base_url('/assets/js/html5shiv.min.js') ?>"></script>
+    <script src="<?php echo base_url('/assets/js/respond.min.js') ?>"></script>
+    <![endif]-->
+    <script src="<?php echo base_url('/assets/js/vendor/modernizr-2.8.3.min.js') ?>"></script>
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+            <form id="recover" action="<?php echo site_url('auth/do_recover') ?>" method="post">
+                <div class="form-group">
+                    <label for="name">Nama</label>
+                    <input type="text" class="form-control" id="name" placeholder="Nama" name="name">
+                </div>
+                <div class="form-group">
+                    <label for="credential">NISN</label>
+                    <input type="number" class="form-control" id="credential" placeholder="NISN" name="credential">
+                </div>
+                <div class="form-group">
+                    <label for="role">Jenis Kelamin</label>
+                    <select id="role" name="gender" class="form-control">
+                        <option value="male">Pria</option>
+                        <option value="female">Wanita</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label for="created">Tanggal Pembuatan Akun</label>
+                    <input type="text" class="form-control" id="created" name="created"/>
+                </div>
+                <input type="hidden" name="role" value="student">
+                <button type="submit" class="btn btn-default">Recover</button>
+            </form>
+        </div>
+    </div>
+</div>
+
+<script src="<?php echo base_url('/assets/bower_components/jquery/dist/jquery.min.js') ?>"></script>
+<script>window.jQuery || document.write('<script src="<?php echo base_url('/assets/bower_components/jquery/dist/jquery.min.js') ?>"><\/script>')</script>
+<script src="<?php echo base_url('/assets/js/plugins.js') ?>"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script type="text/javascript" src="<?php echo base_url('/assets/bower_components/bootstrap/dist/js/bootstrap.min.js') ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('/assets/bower_components/tether/dist/js/tether.min.js') ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('/assets/bower_components/jquery-serialize-object/dist/jquery.serialize-object.min.js') ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('/assets/bower_components/remarkable-bootstrap-notify/dist/bootstrap-notify.min.js') ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('/assets/bower_components/moment/min/moment.min.js') ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('/assets/bower_components/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js') ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('/assets/js/auth/recover-student.min.js') ?>"></script>
+
+</body>
+</html>
