d92c249b3f9f57bcf203b98f348acce01ea86815
Syafiqq <syafiq.rezpector@gmail.com>
2017-05-02 11:33:58 +0700

    Add minify js file watcher

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var watch = require('gulp-watch');
 var cleanCSS = require('gulp-clean-css');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
@@ -16,6 +17,24 @@ gulp.task('minify-js', function (cb)
         ],
         cb
     );
+});
+
+gulp.task('watch-minify-js', function ()
+{
+    // Callback mode, useful if any plugin in the pipeline depends on the `end`/`flush` event
+    return watch('raw/js/**/*.js', function (cb)
+    {
+        pump([
+                gulp.src('raw/js/**/*.js').pipe(rename({
+                    suffix: ".min",
+                    extname: ".js"
+                })),
+                uglify(),
+                gulp.dest('assets/js')
+            ],
+            cb
+        );
+    });
 });
 
 gulp.task('minify-css', function ()
