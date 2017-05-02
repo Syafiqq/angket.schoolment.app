40252dca5c1b612d6e37077bd9fba132d477fe4f
Syafiqq <syafiq.rezpector@gmail.com>
2017-05-02 11:37:15 +0700

    Extending authentication feature - Add forgot password functionality

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -6,6 +6,8 @@
  * Email        : syafiq.rezpector@gmail.com
  * Github       : syafiqq
  */
+use Carbon\Carbon;
+
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Auth extends CI_Controller
@@ -103,6 +105,59 @@ class Auth extends CI_Controller
         redirect('/auth/register?role=student');
     }
 
+    public function recover()
+    {
+        if (isset($_GET['role']))
+        {
+            if (isset($_GET['token']))
+            {
+
+                switch ($_GET['role'])
+                {
+                    case 'counselor' :
+                    {
+                        $this->load->view('auth/recover/recover-counselor-confirm', ['token' => urldecode($_GET['token'])]);
+
+                        return;
+                    }
+                    case 'student' :
+                    {
+                        $this->load->view('auth/recover/recover-student-confirm', ['token' => urldecode($_GET['token'])]);
+
+                        return;
+                    }
+                }
+
+                return;
+            }
+            else
+            {
+                switch ($_GET['role'])
+                {
+                    case 'counselor' :
+                    {
+                        $this->load->view('auth/recover/recover-counselor');
+
+                        return;
+                    }
+                    case 'student' :
+                    {
+                        $this->load->view('auth/recover/recover-student');
+
+                        return;
+                    }
+                    default:
+                    {
+                        redirect('/auth/recover?role=student');
+                    }
+                }
+
+                return;
+            }
+        }
+        redirect('/auth/recover?role=student');
+    }
+
     public function do_login()
     {
         if ($this->input->is_ajax_request() && ($_SERVER['REQUEST_METHOD'] === 'POST'))
@@ -137,31 +192,31 @@ class Auth extends CI_Controller
                         {
                             $_SESSION['user']['auth'] = $result[0];
                             $_SESSION['user']['auth']['role'] = $_POST['role'];
-                            echo apiMakeCallback(API_SUCCESS, 'Accepted', ['notify' => [['Login Success', 'success']]], site_url('dashboard'));
+                            echo apiMakeCallback(API_SUCCESS, 'Accepted', ['notify' => [['Login Sukses', 'success']]], site_url('dashboard'));
                         }
                         else
                         {
-                            echo apiMakeCallback(API_NOT_ACCEPTABLE, 'Wrong Password', ['notify' => [['Wrong Password', 'info']]]);
+                            echo apiMakeCallback(API_NOT_ACCEPTABLE, 'Password Salah', ['notify' => [['Password Salah', 'info']]]);
                         }
                     }
                     else
                     {
-                        echo apiMakeCallback(API_NOT_ACCEPTABLE, 'User Not Registered', ['notify' => [['User Not Registered', 'info']]]);
+                        echo apiMakeCallback(API_NOT_ACCEPTABLE, 'Akun Belum Terdaftar', ['notify' => [['Akun Belum Terdaftar', 'info']]]);
                     }
                 }
                 else
                 {
-                    echo apiMakeCallback(API_NOT_ACCEPTABLE, 'Invalid Role', ['notify' => [['Invalid Role', 'info']]]);
+                    echo apiMakeCallback(API_NOT_ACCEPTABLE, 'Peran Tidak Diketahui', ['notify' => [['Peran Tidak Diketahui', 'info']]]);
                 }
             }
             else
             {
-                echo apiMakeCallback(API_NOT_ACCEPTABLE, 'Insufficient Data', ['notify' => [['Insufficient Data', 'info']]]);
+                echo apiMakeCallback(API_NOT_ACCEPTABLE, 'Data Kurang Lengkap', ['notify' => [['Data Kurang Lengkap', 'info']]]);
             }
         }
         else
         {
-            echo apiMakeCallback(API_BAD_REQUEST, 'Bad Request', ['notify' => [['Bad Request', 'danger']]]);
+            echo apiMakeCallback(API_BAD_REQUEST, 'Permintaan Tidak Dapat Dikenali', ['notify' => [['Permintaan Tidak Dapat Dikenali', 'danger']]]);
         }
     }
 
@@ -193,11 +248,11 @@ class Auth extends CI_Controller
                                 {
                                     $_POST['password'] = password_hash($_POST['password'], PASSWORD_DEFAULT);
                                     $this->auth->registerStudent($_POST['name'], $_POST['credential'], $_POST['gender'], $_POST['password']);
-                                    echo apiMakeCallback(API_SUCCESS, 'Register Success', ['notify' => [['Register Success', 'success']]], site_url("auth/login?role={$_POST['role']}"));
+                                    echo apiMakeCallback(API_SUCCESS, 'Register Sukses', ['notify' => [['Register Success', 'success']]], site_url("auth/login?role={$_POST['role']}"));
                                 }
                                 else
                                 {
-                                    echo apiMakeCallback(API_NOT_ACCEPTABLE, 'NISN Already Exists', ['notify' => [['NISN Already Exists', 'info']]]);
+                                    echo apiMakeCallback(API_NOT_ACCEPTABLE, 'NISN Sudah Terdaftar', ['notify' => [['NISN Sudah Terdaftar', 'info']]]);
                                 }
                             }
                                 break;
@@ -208,11 +263,11 @@ class Auth extends CI_Controller
                                 {
                                     $_POST['password'] = password_hash($_POST['password'], PASSWORD_DEFAULT);
                                     $this->auth->registerCounselor($_POST['name'], $_POST['credential'], $_POST['gender'], $_POST['password']);
-                                    echo apiMakeCallback(API_SUCCESS, 'Register Success', ['notify' => [['Register Success', 'success']]], site_url("auth/login?role={$_POST['role']}"));
+                                    echo apiMakeCallback(API_SUCCESS, 'Register Sukses', ['notify' => [['Register Sukses', 'success']]], site_url("auth/login?role={$_POST['role']}"));
                                 }
                                 else
                                 {
-                                    echo apiMakeCallback(API_NOT_ACCEPTABLE, 'NIP/NIK Already Exists', ['notify' => [['NIP/NIK Already Exists', 'info']]]);
+                                    echo apiMakeCallback(API_NOT_ACCEPTABLE, 'NIP/NIK Sudah Terdaftar', ['notify' => [['NIP/NIK Sudah Terdaftar', 'info']]]);
                                 }
                             }
                                 break;
@@ -220,24 +275,160 @@ class Auth extends CI_Controller
                     }
                     else
                     {
-                        echo apiMakeCallback(API_NOT_ACCEPTABLE, 'Invalid Gender', ['notify' => [['Invalid Gender', 'info']]]);
+                        echo apiMakeCallback(API_NOT_ACCEPTABLE, 'Jenis Kelamin Tidak Diketahui', ['notify' => [['Jenis Kelamin Tidak Diketahui', 'info']]]);
                     }
                 }
                 else
                 {
-                    echo apiMakeCallback(API_NOT_ACCEPTABLE, 'Invalid Role', ['notify' => [['Invalid Role', 'info']]]);
+                    echo apiMakeCallback(API_NOT_ACCEPTABLE, 'Peran Tidak Diketahui', ['notify' => [['Peran Tidak Diketahui', 'info']]]);
                 }
             }
             else
             {
-                echo apiMakeCallback(API_NOT_ACCEPTABLE, 'Insufficient Data', ['notify' => [['Insufficient Data', 'info']]]);
+                echo apiMakeCallback(API_NOT_ACCEPTABLE, 'Data Kurang Lengkap', ['notify' => [['Data Kurang Lengkap', 'info']]]);
             }
         }
         else
         {
-            echo apiMakeCallback(API_BAD_REQUEST, 'Bad Request', ['notify' => [['Bad Request', 'danger']]]);
+            echo apiMakeCallback(API_BAD_REQUEST, 'Permintaan Tidak Dapat Dikenali', ['notify' => [['Permintaan Tidak Dapat Dikenali', 'danger']]]);
+        }
+    }
+
+    public function do_recover()
+    {
+        if ($this->input->is_ajax_request() && ($_SERVER['REQUEST_METHOD'] === 'POST'))
+        {
+            if (isset($_POST['name']) &&
+                isset($_POST['credential']) &&
+                isset($_POST['role']) &&
+                isset($_POST['gender']) &&
+                isset($_POST['created'])
+            )
+            {
+                $_POST['credential'] = strtolower($_POST['credential']);
+                $_POST['role'] = strtolower($_POST['role']);
+                if (($_POST['role'] == 'student') || ($_POST['role'] == 'counselor'))
+                {
+                    $_POST['gender'] = strtolower($_POST['gender']);
+                    if (($_POST['gender'] == 'male') || ($_POST['gender'] == 'female'))
+                    {
+                        $this->load->model('mauth', 'auth');
+                        switch ($_POST['role'])
+                        {
+                            case 'student' :
+                            {
+                                $account = $this->auth->findStudentByCredential($_POST['credential']);
+                                if (count($account) <= 0)
+                                {
+                                    echo apiMakeCallback(API_NOT_ACCEPTABLE, 'NISN Belum Terdaftar', ['notify' => [['NISN Belum Terdaftar', 'info']]]);
+
+                                    return;
+                                }
+                            }
+                                break;
+                            case 'counselor' :
+                            {
+                                $account = $this->auth->findCounselorByCredential($_POST['credential']);
+                                if (count($account) <= 0)
+                                {
+                                    echo apiMakeCallback(API_NOT_ACCEPTABLE, 'NIP/NIK Belum Terdaftar', ['notify' => [['NIP/NIK Belum Terdaftar', 'info']]]);
+
+                                    return;
+                                }
+                            }
+                        }
+                        $account = $account[0];
+                        $_created = Carbon::createFromFormat('Y-m-d', $_POST['created']);
+                        $_createdDB = Carbon::createFromFormat('Y-m-d H:i:s', $account['on_create']);
+                        if (($_POST['name'] === $account['name']) &&
+                            ($_POST['gender'] === $account['gender']) &&
+                            ($_created->isSameDay($_createdDB))
+                        )
+                        {
+                            $token = base64_encode(bin2hex(random_bytes(32)));
+                            $this->load->model('mrecovery', 'recovery');
+                            $recovery = $this->recovery->getByUserIdAndRole($account['id'], $_POST['role']);
+                            if (count($recovery) > 0)
+                            {
+                                $this->recovery->updateToken($recovery[0]['id'], $token);
+                            }
+                            else
+                            {
+                                $this->recovery->create($account['id'], $_POST['role'], $token);
+                            }
+                            $token = urlencode($token);
+                            echo apiMakeCallback(API_SUCCESS, 'User Terdaftar', ['notify' => [['User Terdaftar', 'success']]], site_url("auth/recover?role={$_POST['role']}&token={$token}"));
+                        }
+                        else
+                        {
+                            echo apiMakeCallback(API_NOT_ACCEPTABLE, 'Data Tidak Benar', ['notify' => [['Data Tidak Benar', 'info']]]);
+                        }
+                    }
+                    else
+                    {
+                        echo apiMakeCallback(API_NOT_ACCEPTABLE, 'Jenis Kelamin Tidak Diketahui', ['notify' => [['Jenis Kelamin Tidak Diketahui', 'info']]]);
+                    }
+                }
+                else
+                {
+                    echo apiMakeCallback(API_NOT_ACCEPTABLE, 'Peran Tidak Diketahui', ['notify' => [['Peran Tidak Diketahui', 'info']]]);
+                }
+            }
+            else
+            {
+                echo apiMakeCallback(API_NOT_ACCEPTABLE, 'Data Kurang Lengkap', ['notify' => [['Data Kurang Lengkap', 'info']]]);
+            }
+        }
+        else
+        {
+            echo apiMakeCallback(API_BAD_REQUEST, 'Permintaan Tidak Dapat Dikenali', ['notify' => [['Permintaan Tidak Dapat Dikenali', 'danger']]]);
         }
     }
 
 
+    public function do_recover_confirm()
+    {
+        if ($this->input->is_ajax_request() && ($_SERVER['REQUEST_METHOD'] === 'POST'))
+        {
+            if (isset($_POST['password']) &&
+                isset($_POST['token'])
+            )
+            {
+                $this->load->model('mrecovery', 'recovery');
+                $recovery = $this->recovery->getByToken($_POST['token']);
+                if (count($recovery) > 0)
+                {
+                    $this->load->model('mauth', 'auth');
+                    $recovery = $recovery[0];
+                    $_POST['password'] = password_hash($_POST['password'], PASSWORD_DEFAULT);
+                    switch ($recovery['role'])
+                    {
+                        case 'student' :
+                        {
+                            $this->auth->updatePasswordStudentByID($recovery['user'], $_POST['password']);
+                        }
+                            break;
+                        case 'counselor' :
+                        {
+                            $this->auth->updatePasswordCounselorByID($recovery['user'], $_POST['password']);
+                        }
+                    }
+                    $this->recovery->deleteById($recovery['id']);
+                    echo apiMakeCallback(API_SUCCESS, 'Update Berhasil', ['notify' => [['Update Berhasil', 'success']]], site_url("auth/login?role={$recovery['role']}"));
+                }
+                else
+                {
+                    echo apiMakeCallback(API_NOT_ACCEPTABLE, 'Token Belum Terdaftar', ['notify' => [['Token Belum Terdaftar', 'info']]]);
+                }
+            }
+            else
+            {
+                echo apiMakeCallback(API_NOT_ACCEPTABLE, 'Data Kurang Lengkap', ['notify' => [['Data Kurang Lengkap', 'info']]]);
+            }
+        }
+        else
+        {
+            echo apiMakeCallback(API_BAD_REQUEST, 'Permintaan Tidak Dapat Dikenali', ['notify' => [['Permintaan Tidak Dapat Dikenali', 'danger']]]);
+        }
+    }
 }
