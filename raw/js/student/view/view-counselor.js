320e4ebe6c25f70eebd983080a0aa0e522bfcc53
Syafiqq <syafiq.rezpector@gmail.com>
2017-05-06 13:45:18 +0700

    Add student view counselor

---
+++ raw/js/student/view/view-counselor.js
@@ -0,0 +1,111 @@
+/**
+ * This <angket.000.app> project created by :
+ * Name         : syafiq
+ * Date / Time  : 06 May 2017, 10:49 AM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+
+(function ($)
+{
+    $(function ()
+    {
+        $("button.do-active").on('click', function ()
+        {
+            var jsonObj = {};
+            jsonObj.id = $(this).attr('data-student-id');
+            $.ajax({
+                type: 'post',
+                url: $(this).attr('data-student-action'),
+                data: jsonObj,
+                dataType: 'json',
+                contentType: 'application/x-www-form-urlencoded; charset=UTF-8; X-Requested-With: XMLHttpRequest'
+            })
+                .done(function (data)
+                {
+                    if (data.hasOwnProperty('data'))
+                    {
+                        if (data['data'].hasOwnProperty('notify'))
+                        {
+                            var notify = data['data']['notify'];
+                            for (var i = -1; ++i < notify.length;)
+                            {
+                                $.notify({message: notify[i][0]}, {type: notify[i][1]});
+                            }
+                        }
+                    }
+                    if (data.hasOwnProperty('code'))
+                    {
+                        if (data['code'] === "200")
+                        {
+                            if (data.hasOwnProperty('redirect'))
+                            {
+                                setTimeout(function ()
+                                {
+                                    location.href = data['redirect'];
+                                }, 2000);
+                            }
+                        }
+                    }
+                })
+                .fail(function ()
+                {
+                    $.notify({
+                        message: 'Error', url: window.location.protocol + "//" + window.location.host
+                    }, {
+                        type: 'danger'
+                    });
+                })
+
+        });
+
+        $("a#logout").on('click', function (event)
+        {
+            event.preventDefault();
+            $.ajax({
+                type: 'post',
+                url: $(this).attr('href'),
+                dataType: 'json',
+                contentType: 'application/x-www-form-urlencoded; charset=UTF-8; X-Requested-With: XMLHttpRequest'
+            })
+                .done(function (data)
+                {
+                    if (data.hasOwnProperty('data'))
+                    {
+                        if (data['data'].hasOwnProperty('notify'))
+                        {
+                            var notify = data['data']['notify'];
+                            for (var i = -1; ++i < notify.length;)
+                            {
+                                $.notify({message: notify[i][0]}, {type: notify[i][1]});
+                            }
+                        }
+                    }
+                    if (data.hasOwnProperty('code'))
+                    {
+                        if (data['code'] === "200")
+                        {
+                            if (data.hasOwnProperty('redirect'))
+                            {
+                                setTimeout(function ()
+                                {
+                                    location.href = data['redirect'];
+                                }, 2000);
+                            }
+                        }
+                    }
+                })
+                .fail(function ()
+                {
+                    $.notify({
+                        message: 'Error', url: window.location.protocol + "//" + window.location.host
+                    }, {
+                        type: 'danger'
+                    });
+                })
+        });
+    });
+    /*
+     * Run right away
+     * */
+})(jQuery);
