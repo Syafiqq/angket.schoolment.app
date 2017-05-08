aa0c1228d3eeabe647794717960d65ad25e20110
Syafiqq <syafiq.rezpector@gmail.com>
2017-05-08 18:59:48 +0700

    Fix route

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,6 +50,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['default_controller'] = 'welcome';
-$route['inventory/question/edit/(:num)'] = 'inventory/edit_question/$1';
+$route['inventory/edit/(:num)'] = 'inventory/edit/$1';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = false;
