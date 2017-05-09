9f6bd755da8f79dd58e267d41c380c5ba53a4ed6
Syafiqq <syafiq.rezpector@gmail.com>
2017-05-09 16:11:16 +0700

    Add counselor student detail page

---
+++ raw/js/student/detail/counselor-detail-student.js
@@ -0,0 +1,65 @@
+/**
+ * This <angket.bkritis.app> project created by :
+ * Name         : syafiq
+ * Date / Time  : 09 May 2017, 3:56 PM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+
+(function ($)
+{
+    $('table#detail_tb').DataTable({
+        paging: false
+    });
+    $(function ()
+    {
+        $("a#logout").on('click', function (event)
+        {
+            event.preventDefault();
+            $.ajax({
+                type: 'post',
+                url: $(this).attr('href'),
+                dataType: 'json',
+                contentType: 'application/x-www-form-urlencoded; charset=UTF-8; X-Requested-With: XMLHttpRequest'
+            })
+                .done(function (data)
+                {
+                    if (data.hasOwnProperty('data'))
+                    {
+                        if (data['data'].hasOwnProperty('notify'))
+                        {
+                            var notify = data['data']['notify'];
+                            for (var i = -1; ++i < notify.length;)
+                            {
+                                $.notify({message: notify[i][0]}, {type: notify[i][1]});
+                            }
+                        }
+                    }
+                    if (data.hasOwnProperty('code'))
+                    {
+                        if (data['code'] === "200")
+                        {
+                            if (data.hasOwnProperty('redirect'))
+                            {
+                                setTimeout(function ()
+                                {
+                                    location.href = data['redirect'];
+                                }, 2000);
+                            }
+                        }
+                    }
+                })
+                .fail(function ()
+                {
+                    $.notify({
+                        message: 'Error', url: window.location.protocol + "//" + window.location.host
+                    }, {
+                        type: 'danger'
+                    });
+                })
+        });
+    });
+    /*
+     * Run right away
+     * */
+})(jQuery);
---
+++ raw/views/student/detail/counselor-detail-student.php
@@ -0,0 +1,173 @@
+<?php
+/**
+ * This <angket.bkritis.app> project created by :
+ * Name         : syafiq
+ * Date / Time  : 09 May 2017, 3:54 PM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+use Carbon\Carbon;
+
+setlocale(LC_TIME, 'id_ID');
+
+if (!isset($answered))
+{
+    $answered = [];
+}
+
+if (!isset($categories))
+{
+    $categories = [];
+}
+$profile['birthplace'] = $profile['birthplace'] === null ? '-' : $profile['birthplace'];
+$profile['datebirth'] = $profile['datebirth'] === null ? '-' : Carbon::createFromFormat('Y-m-d', $profile['datebirth'])->formatLocalized('%d %B %Y');
+$profile['birth'] = (($profile['birthplace'] === '-') && ($profile['datebirth'] === '-')) ? '-' : (($profile['birthplace'] === '-') ? $profile['datebirth'] : (($profile['datebirth'] === '-') ? $profile['birthplace'] : "{$profile['birthplace']}, {$profile['datebirth']}"));
+
+?>
+
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Daftar Siswa</title>
+    <meta name="a temlplate" content="">
+
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo base_url('/apple-touch-icon.png') ?>">
+    <!-- Place favicon.ico in the root directory -->
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="<?php echo base_url('/assets/css/normalize.min.css') ?>">
+    <link rel="stylesheet" href="<?php echo base_url('/assets/css/main.min.css') ?>">
+
+    <link href="<?php echo base_url('/assets/bower_components/bootstrap/dist/css/bootstrap.min.css') ?>" rel="stylesheet">
+    <link href="<?php echo base_url('/assets/bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css') ?>" rel="stylesheet">
+    <link href="<?php echo base_url('/assets/bower_components/datatables-autofill-bootstrap/css/autoFill.bootstrap.min.css') ?>" rel="stylesheet">
+    <link href="<?php echo base_url('/assets/bower_components/datatables.net-buttons-bs/css/buttons.bootstrap.min.css') ?>" rel="stylesheet">
+    <link href="<?php echo base_url('/assets/css/student/detail/counselor-detail-student.min.css') ?>" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="<?php echo base_url('/assets/js/html5shiv.min.js') ?>"></script>
+    <script src="<?php echo base_url('/assets/js/respond.min.js') ?>"></script>
+    <![endif]-->
+    <script src="<?php echo base_url('/assets/js/vendor/modernizr-2.8.3.min.js') ?>"></script>
+</head>
+<body>
+<nav class="navbar navbar-default">
+    <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="<?php echo site_url('dashboard') ?>">Site</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li>
+                    <a href="<?php echo site_url('inventory') ?>">Inventory</a>
+                </li>
+                <li>
+                    <a href="<?php echo site_url('student') ?>">Siswa</a>
+                </li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li>
+                    <a id="logout" href="<?php echo site_url('auth/do_logout') ?>">Logout</a>
+                </li>
+                <li>
+                    <a href="<?php echo site_url('profile') ?>">Profile</a>
+                </li>
+            </ul>
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+</nav>
+
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">Identitas</div>
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <dl class="dl-horizontal" style="margin-top: 50px">
+                                <dt>Nama</dt>
+                                <dd><?php echo ": {$profile['name']}"?></dd>
+                                <dt>NISN</dt>
+                                <dd><?php echo ": {$profile['credential']}"?></dd>
+                                <dt>Kelas</dt>
+                                <dd><?php echo ": {$profile['grade']}"?></dd>
+                                <dt>Sekolah</dt>
+                                <dd><?php echo ": {$profile['school']}"?></dd>
+                                <dt>TTL</dt>
+                                <dd><?php echo ": {$profile['birth']}"?></dd>
+                            </dl>
+                        </div>
+                    </div>
+                <div class="panel-body">
+                </div>
+                <div class="table-responsive">
+                    <table id="detail_tb" class="table table-hover">
+                        <thead>
+                        <tr>
+                            <th style="width: 40px">No</th>
+                            <th style="width: 150px">Pengisian</th>
+                            <?php foreach ($categories as $cv)
+                                echo "<th style=\"width: 150px\">{$cv['name']}</th>"
+                            ?>
+                            <th style="width: 100px">Tampilkan</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <?php
+                        $no = 0;
+                        foreach ($answered as $av)
+                        {
+                            ++$no;
+                            $av['answer_at'] = Carbon::createFromFormat('Y-m-d H:i:s', $av['answer_at'])->formatLocalized('%d %B %Y %H:%M');
+                            echo '<tr>';
+                            echo "<td>{$no}</td>";
+                            echo "<td>{$av['answer_at']}</td>";
+                            foreach ($categories as $cv)
+                            {
+                                $cid = ".{$cv['id']}";
+                                printf('<td>%.4f %%</td>', $av['category'][$cid]);
+                            }
+                            $url = site_url("report/publish?answer={$av['id']}");
+                            echo "<td><a class=\"btn btn-default\" href=\"{$url}\" role=\"button\">Cetak</a></td>";
+                            echo '</tr>';
+                        }
+                        ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script src="<?php echo base_url('/assets/bower_components/jquery/dist/jquery.min.js') ?>"></script>
+<script>window.jQuery || document.write('<script src="<?php echo base_url('/assets/bower_components/jquery/dist/jquery.min.js') ?>"><\/script>')</script>
+<script src="<?php echo base_url('/assets/js/plugins.min.js') ?>"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="<?php echo base_url('/assets/bower_components/bootstrap/dist/js/bootstrap.min.js') ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('/assets/bower_components/tether/dist/js/tether.min.js') ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('/assets/bower_components/remarkable-bootstrap-notify/dist/bootstrap-notify.min.js') ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('/assets/bower_components/datatables.net/js/jquery.dataTables.min.js') ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('/assets/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js') ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('/assets/bower_components/datatables.net-autofill/js/dataTables.autoFill.min.js') ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('/assets/bower_components/datatables-autofill-bootstrap/js/autoFill.bootstrap.min.js') ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('/assets/bower_components/datatables.net-buttons/js/dataTables.buttons.js') ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('/assets/bower_components/datatables.net-buttons-bs/js/buttons.bootstrap.min.js') ?>"></script>
+<script src="<?php echo base_url('/assets/js/student/detail/counselor-detail-student.min.js') ?>"></script>
+</body>
+</html>
