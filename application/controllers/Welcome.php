632166adc9a620c6a013f45cb3e2abae3223d616
Syafiqq <syafiq.rezpector@gmail.com>
2017-05-10 18:34:40 +0700

    Fix landing page route

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -21,7 +21,9 @@ class Welcome extends CI_Controller
      */
     public function index()
     {
-        $this->load->view('welcome_message');
+        $this->load->helper('url');
+
+        $this->load->view('landing_page');
     }
 
     public function boilerplate()
