d7ee48f8480e8989ef197d4d5f1e4612d8c5d082
Syafiqq <syafiq.rezpector@gmail.com>
2017-05-12 20:29:45 +0700

    Batch1

---
+++ raw/views/baked/cardiovaskular.php
@@ -0,0 +1,306 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Inventori School Engagement siswa SMP</title>
+    <meta name="description" content="Cardio is a free one page template made exclusively for Codrops by Luka Cvetinovic" />
+    <meta name="keywords" content="html template, css, free, one page, gym, fitness, web design" />
+    <meta name="author" content="Luka Cvetinovic for Codrops" />
+    <!-- Favicons (created with http://realfavicongenerator.net/)-->
+    <link rel="apple-touch-icon" sizes="57x57" href="/assets/baked/cardiovaskular/img/favicons/apple-touch-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="/assets/baked/cardiovaskular/img/favicons/apple-touch-icon-60x60.png">
+    <link rel="icon" type="image/png" href="/assets/baked/cardiovaskular/img/favicons/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="/assets/baked/cardiovaskular/img/favicons/favicon-16x16.png" sizes="16x16">
+    <link rel="manifest" href="/assets/baked/cardiovaskular/img/favicons/manifest.json">
+    <link rel="shortcut icon" href="/assets/baked/cardiovaskular/img/favicons/favicon.ico">
+    <meta name="msapplication-TileColor" content="#00a8ff">
+    <meta name="msapplication-config" content="/assets/baked/cardiovaskular/img/favicons/browserconfig.xml">
+    <meta name="theme-color" content="#ffffff">
+    <!-- Normalize -->
+    <link rel="stylesheet" type="text/css" href="/assets/baked/cardiovaskular/css/normalize.css">
+    <!-- Bootstrap -->
+    <link rel="stylesheet" type="text/css" href="/assets/baked/cardiovaskular/css/bootstrap.css">
+    <!-- Owl -->
+    <link rel="stylesheet" type="text/css" href="/assets/baked/cardiovaskular/css/owl.css">
+    <!-- Animate.css -->
+    <link rel="stylesheet" type="text/css" href="/assets/baked/cardiovaskular/css/animate.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" type="text/css" href="/assets/baked/cardiovaskular/fonts/font-awesome-4.1.0/css/font-awesome.min.css">
+    <!-- Elegant Icons -->
+    <link rel="stylesheet" type="text/css" href="/assets/baked/cardiovaskular/fonts/eleganticons/et-icons.css">
+    <!-- Main style -->
+    <link rel="stylesheet" type="text/css" href="/assets/baked/cardiovaskular/css/cardio.css">
+</head>
+
+<body>
+<div class="preloader">
+    <img src="/assets/baked/cardiovaskular/img/loader.gif" alt="Preloader image">
+</div>
+<nav class="navbar">
+    <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#"><img src="/assets/baked/cardiovaskular/img/logo.png" data-active-url="/assets/baked/cardiovaskular/img/logo-active.png" alt=""></a>
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right main-nav">
+                <li><a href="#intro">Home</a></li>
+                <li><a href="#services">Pengantar</a></li>
+                <li><a href="#team">pengembang</a></li>
+                <li><a href="<?php echo site_url('/auth/register?role=student')?>"  class="btn btn-blue">Daftar Akun</a></li>
+                <li><a href="<?php echo site_url('/auth/login?role=student')?>"  class="btn btn-blue">Masuk Akun</a></li>
+            </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container-fluid -->
+</nav>
+<header id="intro">
+    <div class="container">
+        <div class="table">
+            <div class="header-text">
+                <div class="row">
+                    <div class="col-md-12 text-center">
+                        <h3 class="light white">Software Invontori Bimbingan dan Konseling</h3>
+                        <h1 class="white typed">Inventori School Engagement Siswa SMP </h1>
+                        <span class="typed-cursor">|</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+
+<section id="services" class="section section-padded">
+    <div class="container">
+        <div class="row text-center title">
+            <h2>School Engagement siswa SMP</h2>
+
+            <p> Menurut Fredricks (dalam Afriyanti, 2014) school engagement adalah keterlibatan siswa pada kegiatan akademik dan non akademik yang tampak pada tingkah laku, emosi, dan kognitif yang ditampilakan siswa disekolah dan kelas. School Engagement dalam topik kami adalah school engagment siswa di dalam kelas dengan tiga dimendi yang menjadi karakteristik, yaitu:</p>
+        </div>
+
+        <div class="row services">
+            <div class="col-md-4">
+                <div class="service">
+                    <div class="icon-holder">
+                        <img src="/assets/baked/cardiovaskular/img/icons/heart-blue.png" alt="" class="icon">
+                    </div>
+                    <h4 class="heading">Behavioral engagement</h4>
+                    <p class="description" style="font-size: 14px">merupakan bentuk tindakan siswa yang berupa partisipasi secara positif maupun negatif  dalam mengikuti kegiatan disekolah, konsistensi perilaku yang menggambarkan usaha,  konsentrasi dan perhatian, mengikuti peraturan sekolah, menyelesaikan tugas yang diberikan, tidak mengganggu teman, dan tidak membuat keonaran di sekolah.</p>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="service">
+                    <div class="icon-holder">
+                        <img src="/assets/baked/cardiovaskular/img/icons/guru-blue.png" alt="" class="icon">
+                    </div>
+                    <h4 class="heading">Emotional Engagement</h4>
+                    <p class="description" style="font-size: 14px">merupakan reaksi positif atau negatif siswa terhadap guru, teman sekelas, kegiatan akademik dan sekolah, reaksi afektif di dalam kelas,  sikap menerima terhadap sekolah dan guru, mengidentifikasikan diri dengan sekolah, perasaan memiliki, serta apresiasi keberhasilan di sekolah.</p>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="service">
+                    <div class="icon-holder">
+                        <img src="/assets/baked/cardiovaskular/img/icons/weight-blue.png" alt="" class="icon">
+                    </div>
+                    <h4 class="heading">Cognitive Engagement</h4>
+                    <p class="description" style="font-size: 14px">Penggabungan perhatian dan kemauan siswa untuk mengarahkan upaya yang diperlukan guna memahami suatu materi yang kompleks dan penguasaan terhadap suatu keterampilan yang sulit. fleksibilitas dalam pemecahan masalah, kemauan untuk kerja keras, usaha mental, dan keinginan untuk menguasai materi pelajaran. </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="cut cut-bottom"></div>
+</section>
+<section id="team" class="section gray-bg">
+    <div class="container">
+        <div class="row title text-center">
+            <h2 class="margin-top">Tim Pengembang</h2>
+            <h4 class="light muted">We're a dream team!</h4>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="team text-center">
+                    <div class="cover" style="background:url('/assets/baked/cardiovaskular/img/team/prof-c.jpg'); background-size:cover;">
+                        <div class="overlay text-center">
+                            <h3 class="white">Prof. Dr. Hj. Nur Hidayah M.Pd</h3>
+                            <h5 class="light light-white">Dosen Pembimbing</h5>
+                        </div>
+                    </div>
+                    <img src="/assets/baked/cardiovaskular/img/team/prof.jpg" alt="Team Image" class="avatar">
+                    <div class="title">
+                        <p>Lahir di Gresik, 17 Agustus 1959 dan Bertempat tinggal di Perum Permata Hijau D/57. <br> Memiliki Hobby Membaca dan Motto “ Be Your Self”. <br> Telah selesai menempuh Pendidikan S-1, S-2, S-3 jurusan Bimbingan dan Konseling dan telah menjadi dosen dan guru besar Bimbingan dan Konseling Universitas Megeri Malang </p>
+                        Email          : nurhidayahum@yahoo.com
+                    </div>
+
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="team text-center">
+                    <div class="cover" style="background:url('/assets/baked/cardiovaskular/img/team/fen-c.jpg'); background-size:cover;">
+                        <div class="overlay text-center">
+                            <h3 class="white">Feny Andriyanti</h3>
+                            <h5 class="light light-white">Asisten Dosen</h5>
+                        </div>
+                    </div>
+                    <img src="/assets/baked/cardiovaskular/img/team/fen.jpg" alt="Team Image" class="avatar">
+                    <div class="title">
+                        <p>Lahir di Sidoarjo, 17 Juni 1995 dan beralamat di Ds. Janti, Kec. Tarik,Kab.Sidoarjo. <br> Memiliki Hobby Browsing dan <br> Motto Inginkan yang Terbaik dan Lakukan yang Terbaik</p>
+                        Email       :Feny.andriyanti95@gmail.com
+                    </div>
+
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="team text-center">
+                    <div class="cover" style="background:url('/assets/baked/cardiovaskular/img/team/lia-c.jpg'); background-size:cover;">
+                        <div class="overlay text-center">
+                            <h3 class="white">Maghfirotul Amalia</h3>
+                            <h5 class="light light-white">Asisten Dosen</h5>
+                        </div>
+                    </div>
+                    <img src="/assets/baked/cardiovaskular/img/team/lia.jpg" alt="Team Image" class="avatar">
+                    <div class="title">
+
+                        <p>Lahir di Malang, 06 April 1995 dan beralamat Jl Raya Pang. Sudirman No.74 Bululawang-Malang. <br> Memiliki Hobi berwisata kuliner dan <br> Motto Selalu Berpikir Besar dan Bertindak Mulai Sekarang</p>
+                        Email     : amalia_maghfirotul@yahoo.com
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="team text-center">
+                    <div class="cover" style="background:url('/assets/baked/cardiovaskular/img/team/yur-cc.jpg'); background-size:cover;">
+                        <div class="overlay text-center">
+                            <h3 class="white">Moh. Yuris Siaruddin </h3>
+                            <h5 class="light light-white">Tim Pengembang</h5>
+                        </div>
+                    </div>
+                    <img src="/assets/baked/cardiovaskular/img/team/yur.jpg" alt="Team Image" class="avatar">
+                    <div class="title">
+                        <p>Lahir di Blitar, 03 April 1997 dan Bertempat tinggal di Jl. Telaga Rambit Rt.03/Rw.03 Ds. Purwodadi Sidayu Gresik. <br> Memiliki Hobby Membaca Artikel idntimes.com. <br> Motto "Do’a tanpa usaha adalah kebohongan, Usaha tanpa Do’a adalah Kesombongan, Do’a dan Usaha saling beriringan adalah barokah".</p>
+                        <h6 class="muted regular"></h6>
+                        Email		: siaruddin.yuris@gmail.com
+                    </div>
+
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="team text-center">
+                    <div class="cover" style="background:url('/assets/baked/cardiovaskular/img/team/al-c.jpg'); background-size:cover;">
+                        <div class="overlay text-center">
+                            <h3 class="white">A’liyatul Rosyidah </h3>
+                            <h5 class="light light-white">Tim Pengembang</h5>
+                        </div>
+                    </div>
+                    <img src="/assets/baked/cardiovaskular/img/team/al.jpg" alt="Team Image" class="avatar">
+                    <div class="title">
+                        <p> Lahir di Lamongan, 05 November 1996 dan beralamat di Ds. Sumberwudi Rt.04/Rw.01 Karanggeneng  Lamongan. <br> Memiliki Hobby Memasak dan Bersedekah. <br> Motto "Ingat 2 Hal Lupakan 2 Hal"</p>
+                        Email		: rosyidah.alya@gmail.com
+
+                    </div>
+
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="team text-center">
+                    <div class="cover" style="background:url('/assets/baked/cardiovaskular/img/team/nin-c.jpg'); background-size:cover;">
+                        <div class="overlay text-center">
+                            <h3 class="white">Ninda Hidayatul Rahmawati</h3>
+                            <h5 class="light light-white">Tim Pengembang</h5>
+                        </div>
+                    </div>
+                    <img src="/assets/baked/cardiovaskular/img/team/nin.jpg" alt="Team Image" class="avatar">
+                    <div class="title">
+
+                        <h5 class="muted regular">Lahir di Blitar, 13 April 1997 dan beralamat di Ds. Langon Rt.01/Rw.02 Ponggok Blitar . Memiliki Hobi Membaca dan Motto Jangan Bakar Kesempatan untuk Kenyamanan Sementara</h5>
+                        Email		: nindarahma123@gmail.com
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="team text-center">
+                    <div class="cover" style="background:url('/assets/baked/cardiovaskular/img/team/team-cover1.jpg'); background-size:cover;">
+                        <div class="overlay text-center">
+                            <h3 class="white">Muhammad Syafiq, S.Kom</h3>
+                            <h5 class="light light-white">Programer</h5>
+                        </div>
+                    </div>
+                    <img src="/assets/img/avatar/male/boy-0.png" width="120" height="120" alt="Team Image" class="avatar">
+                    <div class="title">
+                        <h5 class="muted regular">Lahir di Pasuruan, 16 Desember 1993 dan beralamat di Jl. Apel I No. 449 Bangil, Pasuruan. <br> Memiliki Motto Hidup Bermakna seperti Larry. <br> Telah menyelesaikan Pendidikan S-1 Teknik Informatika Universitas Brawijaya</h5>
+                        Email          : id.muhammad.syafiq@gmail.com
+                    </div>
+
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="team text-center">
+                    <div class="cover" style="background:url('/assets/baked/cardiovaskular/img/team/hh-c.jpg'); background-size:cover;">
+                        <div class="overlay text-center">
+                            <h3 class="white">Husni Hanafi</h3>
+                            <h5 class="light light-white">Programer</h5>
+                        </div>
+                    </div>
+                    <img src="/assets/baked/cardiovaskular/img/team/hh.jpg" alt="Team Image" class="avatar">
+                    <div class="title">
+                        <h5 class="muted regular">Lahir di Pasuruan, 19 Juni 1994 dan beralamat di Jl. Suro No. 40 Bangil, Pasuruan. <br> Memiliki Motto Semua mudah di jalan yang tepat. <br> Telah menyelesaikan Pendidikan S-1 Bimbingan dan Konseling Universitas Negeri Malang</h5>
+                        Email       :hanafihusni.19@gmail.com
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<footer>
+    <div class="container">
+
+        <div class="row bottom-footer text-center-mobile">
+            <div class="col-sm-8">
+                <p>&copy; 2015 All Rights Reserved. Powered by <a href="http://www.phir.co/">PHIr</a> exclusively for <a href="http://tympanus.net/codrops/">Codrops</a></p>
+            </div>
+            <div class="col-sm-4 text-right text-center-mobile">
+                <ul class="social-footer">
+                    <li><a href="http://www.facebook.com/pages/Codrops/159107397912"><i class="fa fa-facebook"></i></a></li>
+                    <li><a href="http://www.twitter.com/codrops"><i class="fa fa-twitter"></i></a></li>
+                    <li><a href="https://plus.google.com/101095823814290637419"><i class="fa fa-google-plus"></i></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- Holder for mobile navigation -->
+<div class="mobile-nav">
+    <ul>
+    </ul>
+    <a href="#" class="close-link"><i class="arrow_up"></i></a>
+</div>
+<!-- Scripts -->
+<script src="/assets/baked/cardiovaskular/js/jquery-1.11.1.min.js"></script>
+<script src="/assets/baked/cardiovaskular/js/owl.carousel.min.js"></script>
+<script src="/assets/baked/cardiovaskular/js/bootstrap.min.js"></script>
+<script src="/assets/baked/cardiovaskular/js/wow.min.js"></script>
+<script src="/assets/baked/cardiovaskular/js/typewriter.js"></script>
+<script src="/assets/baked/cardiovaskular/js/jquery.onepagenav.js"></script>
+<script src="/assets/baked/cardiovaskular/js/main.js"></script>
+</body>
+
+</html>
+
+
