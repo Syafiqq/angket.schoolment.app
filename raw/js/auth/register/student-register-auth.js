f37b485eb8243bc02d68b8ebb07050247d4c29ec
Syafiqq <syafiq.rezpector@gmail.com>
2017-05-13 05:14:22 +0700

    Change student register view

--- raw/js/auth/register/student-register-auth.js
+++ raw/js/auth/register/student-register-auth.js
@@ -13,6 +13,12 @@
         var aj = audiojs.createAll();
     });
 
+    $('input').iCheck({
+        checkboxClass: 'icheckbox_square-blue',
+        radioClass: 'iradio_square-blue',
+        increaseArea: '20%' // optional
+    });
+
     $(function ()
     {
         $("form#register").on('submit', function (event)
