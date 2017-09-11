3004df8491897f9d5d6ef0ceb7c74b8b5daf9018
Syafiqq <id.muhammad.syafiq@gmail.com>
2017-09-11 18:34:53 +0700

    Change to SSL

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://angket.schoolment.app/';
+$config['base_url'] = 'https://angket.schoolment.app/';
 
 /*
 |--------------------------------------------------------------------------
