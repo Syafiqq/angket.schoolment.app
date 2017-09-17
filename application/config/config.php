622eca551c16a2ffa36e57a3beb85d53698c16e5
Syafiqq <id.muhammad.syafiq@gmail.com>
2017-09-18 05:41:51 +0700

    Fix Base url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://angket.schoolment.app/';
+$config['base_url'] = 'https://konseling-school-engagement.esy.es/';
 
 /*
 |--------------------------------------------------------------------------
