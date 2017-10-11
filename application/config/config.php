3bdff1ac1375bc0ef2579792ee233d23705d760f
Syafiqq <id.muhammad.syafiq@gmail.com>
2017-10-11 10:54:57 +0700

    Move to http

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://konseling-school-engagement.esy.es/';
+$config['base_url'] = 'http://konseling-school-engagement.esy.es/';
 
 /*
 |--------------------------------------------------------------------------
